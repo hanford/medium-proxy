3929bc9d17c0d4cb0c69627d6fe8e36f12659bd5
Jack Hanford <jackhanford@gmail.com>
2017-04-17 12:34:33 -1000

    async await

--- index.js
+++ index.js
@@ -4,20 +4,16 @@ const cors = require('micro-cors')()
 
 let data = {}
 
-const fetchPosts = () => {
-  return get('https://medium.com/@jackhanford/latest?format=json')
-    .then(res => res.data)
-    .then(body => {
-      data = body.replace('])}while(1);</x>', '')
-      console.log('got medium data')
-    })
-    .catch(err => {
-      throw err
-    })
+async function fetchPosts () {
+  const res = await get('https://medium.com/@jackhanford/latest?format=json')
+  const resData = await res.data
+  data = await resData.replace('])}while(1);</x>', '')
+
+  return data
 }
 
 fetchPosts()
 
-setInterval(fetchPosts, 30 * 60 * 1000)
+setInterval(fetchPosts, 60 * 60 * 1000)
 
 module.exports = cors((req, res) => send(res, 200, data))
